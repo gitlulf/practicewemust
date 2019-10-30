63339454c31c785efc9dfcd678c5605fc4d4f88b
lembckul <ulf.lembcke@pentasys.de>
2019-10-30 22:13:23 +0100

    HelloWorld

--- src/test/java/de/lulf/practicewemust/helloworld/TestHelloWorld.java
+++ src/test/java/de/lulf/practicewemust/helloworld/TestHelloWorld.java
@@ -1,4 +1,12 @@
 package de.lulf.practicewemust.helloworld;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestHelloWorld {
+
+    @Test
+    public void test1plus1() {
+        Assert.assertEquals(1, 1);
+    }
 }
