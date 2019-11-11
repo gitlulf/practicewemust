e081e6fbafb2e774b4d40900a857835e77e3bf90
lembckul <ulf.lembcke@pentasys.de>
2019-11-11 18:53:37 +0100

    Refactor packages.

--- src/main/java/de/lulf/practicewmust/schiffeversenken/SchiffeUeberlappenSichException.java
+++
@@ -1,9 +0,0 @@
-package de.lulf.practicewmust.schiffeversenken;
-
-public class SchiffeUeberlappenSichException extends Exception {
-
-    public SchiffeUeberlappenSichException(String message) {
-        super(message);
-    }
-
-}
